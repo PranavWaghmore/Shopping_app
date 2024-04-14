4c88c3265b4fb5618fb698fce6791e6513cc97f3
pranav_Waghmore <pranavwaghmore1835@gmail.com>
2024-04-14 08:34:15 +0530

    Initial commit

---
+++ app/src/main/java/com/cscorner/shoppingapp/Data.kt
@@ -0,0 +1,158 @@
+package com.cscorner.shoppingapp
+
+import androidx.annotation.DrawableRes
+
+object Data  {
+   class Demo(
+       @DrawableRes var img:Int ,
+       var name:String ,
+       var disp:String ,
+       var price:String
+   )
+    var datalist = listOf(
+        Demo (
+            R.drawable.baseline_menu_24,
+            name = "Anime Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+            ) ,
+        Demo (
+            R.drawable.b ,
+            name = "Grey Shirt" ,
+            disp = "Formal shirt" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.c ,
+            name = "Indian Suite" ,
+            disp = "Famous Indian dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.d,
+            name = "Blue Suite" ,
+            disp = "Famous Blue dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.e ,
+            name = "Violet Suite" ,
+            disp = "Famous Violet dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.f ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.a ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.b ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.c ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.d,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.e ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.f ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.a ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.b ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.c ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.d,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.e ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.f ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.a ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.b ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.c ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.d,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.e ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        ) ,
+        Demo (
+            R.drawable.f ,
+            name = "Anme Suite" ,
+            disp = "Famous Akatsi dress" ,
+            price = "2000"
+        )
+    )
+}
---
+++ app/src/main/java/com/cscorner/shoppingapp/HOMEUIContent.kt
@@ -0,0 +1,188 @@
+package com.cscorner.shoppingapp
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.drawable.TintAwareDrawable
+import com.cscorner.shoppingapp.ui.theme.pink
+
+@Composable
+fun homeUi() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier
+                    .background(pink)
+                    .fillMaxWidth()
+                    .height(56.dp)
+            ) {
+                buttons(icon = R.drawable.baseline_menu_24)
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Pranav",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.W700
+                        )
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    buttons(icon = R.drawable.baseline_search_24)
+                }
+
+            }
+
+                LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+                    items(Data.datalist) {
+                        ShowSuit(data = it)
+                    }
+
+            }
+        }
+
+    }
+}
+
+
+@Composable
+fun buttons(
+    @DrawableRes icon: Int,
+    tint: Color = Color.Unspecified,
+
+    ) {
+
+    IconButton(onClick = { /*TODO*/ }) {
+        Icon(painter = painterResource(id = icon), contentDescription = "")
+    }
+}
+    @Composable
+    fun ShowSuit(
+        data: Data.Demo
+    ) {
+        Card(
+            shape = RoundedCornerShape(5.dp),
+            modifier = Modifier
+                .width(175.dp)
+                .height(300.dp)
+                .padding(vertical = 5.dp, horizontal = 5.dp)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 5.dp, horizontal = 5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = data.img), contentDescription = "",
+                        modifier = Modifier
+                            .size(109.dp)
+                            .clip(CircleShape),
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Text(
+                        text = data.price, style = TextStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.W400,
+                            color = Color.Red
+                        ),
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = data.name,
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.W300,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center
+
+                        ),
+
+                        )
+                    Text(
+                        text = data.disp,
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.W300,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center
+                        ),
+
+                        )
+                    Button(
+                        onClick = { /*TODO*/ }, modifier = Modifier
+                            .width(91.dp),
+                        shape = RoundedCornerShape(18.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = pink
+                        )
+                    )
+                    {
+                        Text(text = "add" , style = TextStyle(
+                            fontSize = 12.sp ,
+                            fontWeight = FontWeight.W600 ,
+                            color = Color.White
+                           ) ,
+                            textAlign = TextAlign.Center
+                        )
+                    
+
+                }
+            }
+        }
+    }
+}
